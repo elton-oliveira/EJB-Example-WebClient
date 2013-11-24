533631e2cb79aefc09f86ba609bfe53c3eed287d
elton-oliveira <elton.pmp@gmail.com>
2013-11-24 21:20:55 -0200

    Add shopping cart client

---
+++ WebContent/shopping_cart.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<c:import url="header.jsp" />
+	
+	<form action="applicationServlet?controller=br.com.fluentcode.controller.ShoppingCartController" method="POST">
+		Item: <input type="text" name="item" /><br /> 
+		<input type="submit" value="Add" />
+	</form>
+	<form action="applicationServlet?controller=br.com.fluentcode.controller.ShoppingCartController" method="POST">
+		<input type="hidden" name="method" value="finishShopping"/>
+		<input type="submit" value="Finish shopping" />
+	</form>
+	
+	<p>${items}</p>
+		
+	<c:import url="footer.jsp" />
+</body>
+</html>
---
+++ src/br/com/fluentcode/controller/ShoppingCartController.java
@@ -0,0 +1,58 @@
+package br.com.fluentcode.controller;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.fluentcode.ejb.remote.ShoppingCartRemote;
+import br.com.fluentcode.infra.mvc.controller.Controller;
+
+
+public class ShoppingCartController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException, NamingException {
+		
+		if (request.getParameter("method") == null) {
+			ShoppingCartRemote cart = lookupCalculatorRemote();
+
+			String item = request.getParameter("item");
+
+			cart.addItem(item);
+		} else if ("finishShopping".equals(request.getParameter("method"))) {
+             this.finishShopping(request);
+		}
+
+		return "/shopping_cart.jsp";
+	}
+	
+	/**
+	 * 
+	 * TODO ler comentário do método
+	 */
+	private void finishShopping(HttpServletRequest request) throws NamingException{
+		//Não pode fazer lookup aqui não está mantendo o estado (trazendo outro ejb)
+		//Como obtém um statefull ejb em um servlet, já que o stateful não pode ser compartilhado???????
+		ShoppingCartRemote cart = lookupCalculatorRemote();
+		request.setAttribute("items", cart.getItems());
+		cart.finishShopping();
+	}
+
+	private ShoppingCartRemote lookupCalculatorRemote() throws NamingException {
+		Properties p = new Properties();
+		p.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+		p.put(Context.PROVIDER_URL, "remote://localhost:4447");
+		p.put(Context.SECURITY_PRINCIPAL, "elton");
+		p.put(Context.SECURITY_CREDENTIALS, "123");
+		InitialContext ctx = new InitialContext(p);
+		return (ShoppingCartRemote) ctx.lookup("EJB-Example/ShoppingCartBean!br.com.fluentcode.ejb.remote.ShoppingCartRemote");
+	}
+
+}
